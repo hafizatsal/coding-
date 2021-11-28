b81322a39d5bad99afb1ccdfd5ce8c12154efa80
muhamad hafiz atsal <92359416+hafizatsal@users.noreply.github.com>
2021-11-28 23:26:50 +0700

    Add files via upload

---
+++ hello all.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //memasukan library iostream
+using namespace std; //agar tidak menuliskan std berulang
+
+int main(){
+cout << "hello all"; //memanggil hello all di layar
+return 0;
+}
